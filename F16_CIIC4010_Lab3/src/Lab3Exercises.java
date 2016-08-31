35cb2489c88b189d0cc60b87375a4b79abdd5cc9
4010b3 <4010b3@aguacate.ece.uprm.edu>
2016-08-31 10:32:00 -0400

    Changed JFrame title

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Jorge Perez");
+		JFrame myFrame = new JFrame("Jorge I. Perez Florez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(800, 400);
 		myFrame.setSize(200, 200);
